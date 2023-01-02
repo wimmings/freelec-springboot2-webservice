7901ccff45206906c3ffdaf5bdf362feeb7faafe
2020083854 <osy515@hanyang.ac.kr>
2023-01-02 17:06:47 +0900

    Add : post save/update/delete UI

--- src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
+++ src/main/java/com/example/freelecspringboot2webservice/service/posts/PostsService.java
@@ -1,18 +1,17 @@
 package com.example.freelecspringboot2webservice.service.posts;
 
-//import com.example.freelecspringboot2webservice.domain.posts.Posts;
-//import com.example.freelecspringboot2webservice.web.dto.PostsListResponseDto;
-//import com.example.freelecspringboot2webservice.web.dto.PostsResponseDto;
-//import com.example.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
-
 import com.example.freelecspringboot2webservice.domain.posts.Posts;
 import com.example.freelecspringboot2webservice.domain.posts.PostsRepository;
+import com.example.freelecspringboot2webservice.web.dto.PostsListResponseDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsResponseDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsSaveRequestDto;
 import com.example.freelecspringboot2webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -40,19 +39,19 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
-//
-//    @Transactional
-//    public void delete(Long id) {
-//        Posts posts = postsRepository.findById(id)
-//                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-//        postsRepository.delete(posts);
-//    }
-//
-//    @Transactional(readOnly = true)
-//    public List<PostsListResponseDto> findAllDesc() {
-//        return postsRepository.findAllDesc().stream()
-//                .map(PostsListResponseDto::new)
-//                .collect(Collectors.toList());
-//    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
 
