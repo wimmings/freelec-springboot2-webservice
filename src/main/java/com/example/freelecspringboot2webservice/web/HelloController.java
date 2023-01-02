9e8587815ff43a096ac4125f3f54d7b69472b4d1
2020083854 <osy515@hanyang.ac.kr>
2023-01-02 12:32:57 +0900

    Add : initial dto & lombok test

--- src/main/java/com/example/freelecspringboot2webservice/web/HelloController.java
+++ src/main/java/com/example/freelecspringboot2webservice/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.freelecspringboot2webservice.web;
 
+import com.example.freelecspringboot2webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,11 +13,11 @@ public class HelloController {
         return "hello";
     }
 
-//    @GetMapping("/hello/dto")
-//    public HelloResponseDto helloDto(@RequestParam("_name") String name,
-//                                     @RequestParam("_amount") int amount) {
-//
-//        return new HelloResponseDto(name, amount);
-//    }
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("_name") String name,
+                                     @RequestParam("_amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
+    }
 
 }
