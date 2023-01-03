8a12850aeeb8224abb4a726d7f3173abdeecc22b
2020083854 <osy515@hanyang.ac.kr>
2023-01-03 17:37:40 +0900

    Add : google login 연동

---
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package com.example.freelecspringboot2webservice.config.auth;
+
+import com.example.freelecspringboot2webservice.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpSession;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        // 컨트롤러 메서드의 특정 파라미터를 지원하는지 판단
+        // 여기선 파라미터에 @LoginUser 어노테이션이 붙어있고, 파라미터 클래스 타입이 SessionUser.class 인 경우 true 반환
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
+
+        return isLoginUserAnnotation && isUserClass;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // 파라미터에 전달한 개체 생성
+        // 여기선 세션에서 객체를 가져옴
+
+        return httpSession.getAttribute("user");
+    }
+}
