5d8303408a35ea520d36c189f94e6a38b4e07831
Suyoung Oh <ohsusu515@gmail.com>
2023-01-08 18:07:44 +0900

    Fix : SecurityConfig

--- src/main/java/com/example/freelecspringboot2webservice/config/auth/SecurityConfig.java
+++ src/main/java/com/example/freelecspringboot2webservice/config/auth/SecurityConfig.java
@@ -2,41 +2,32 @@ package com.example.freelecspringboot2webservice.config.auth;
 
 import com.example.freelecspringboot2webservice.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//
-//    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                    .authorizeRequests()
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
-                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
-                    .anyRequest().authenticated()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
                 .and()
-                    .logout()
-                        .logoutSuccessUrl("/")
+                .logout()
+                .logoutSuccessUrl("/")
                 .and()
-                    .oauth2Login()
-                    .defaultSuccessUrl("/", true) // 로그인 성공시 메인으로
-                    .userInfoEndpoint()
-                    .userService(customOAuth2UserService);
-        return http.build();
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
     }
 }
